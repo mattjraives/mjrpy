30e230f19e8c7e69cb650ce9dc77cdaa2d1fa886
Matthias Raives <mattjraives@gmail.com>
2018-12-06 13:04:33 -0500

    using relative imports for python3 compatibility

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #
-from mplrc import *
+from .mplrc import *
 #
 import numpy as np
 import scipy as sp
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 #
-from constants import *
-from mplfuncs import *
+from .constants import *
+from .mplfuncs import *
