8b8591146de1276d0f3cf72a08da78327c5962b1
Matthias Raives <mattjraives@gmail.com>
2019-03-09 13:46:21 -0500

    added polar resize & remove deprecated style

--- mplfuncs.py
+++ mplfuncs.py
@@ -17,25 +17,26 @@ def ax_prune(ax,xy,which):
 def prune_x(ax,which):
     ax.xaxis.set_major_locator(MaxNLocator(nbins=len(ax.get_xticklabels())-1,\
                                            prune=which))
-                                           
+
 def prune_y(ax,which):
     ax.yaxis.set_major_locator(MaxNLocator(nbins=len(ax.get_yticklabels())-1,\
                                            prune=which))
 
-def resize(fig,axarr):
+def resize(fig,axarr,polar=False):
   nr,nc = np.atleast_2d(axarr).shape
   nr = nr-1
   nc = nc-1
   Y = np.array([6,11,15,18,20,21])
   X = 9.0*Y[nc]/6.0
-  Y = Y[nr]
+  if polar:
+    Y = X
+  else:
+    Y = Y[nr]
   fig.set_size_inches(X,Y)
   for ax in np.ravel(axarr):
-    ax.grid("on")
+    ax.grid(True)
     ax.minorticks_on()
   B = 0.15*6./Y
   L = 0.125*9./X
   fig.subplots_adjust(left=L,bottom=B)
   return fig,axarr
-  
-  
