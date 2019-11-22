6316b2599fb45f4f07ab279c168ee75eff0dad93
Matthias Jelani Raives <raives.1@halley.astronomy.osu.edu>
2019-11-22 17:08:57 -0500

    better figure limits for square plots

--- mplfuncs.py
+++ mplfuncs.py
@@ -27,10 +27,14 @@ def resize(fig,axarr,polar=False,square=False):
   nr = nr-1
   nc = nc-1
   if square:
-    X = 6.5 * (nc + 1)
-    Y = 6.5 * (nr + 1)
-    B = 0.15/(min((nr,nc))+1)
-    L = 0.15/(min((nr,nc))+1)
+    BL_IN = 1.0
+    TR_IN = 0.3
+    X = BL_IN + (6 * (nc + 1)) + TR_IN
+    Y = BL_IN + (6 * (nr + 1)) + TR_IN
+    B = BL_IN/Y
+    L = BL_IN/X
+    T = 1.0 - (TR_IN/Y)
+    R = 1.0 - (TR_IN/X)
   else:
     Y = np.array([6,11,15,18,20,21])
     X = 9.0*Y[nc]/6.0
@@ -44,5 +48,9 @@ def resize(fig,axarr,polar=False,square=False):
   for ax in np.ravel(axarr):
     ax.grid(True)
     ax.minorticks_on()
+    if square:
+      ax.set_aspect("equal")
   fig.subplots_adjust(left=L,bottom=B)
+  if square:
+    fig.subplots_adjust(left=L,bottom=B,top=T,right=R)
   return fig,axarr
