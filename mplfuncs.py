f3e1cc0883553e1281a6c030bb2e64813e77d243
Matthias Jelani Raives <raives.1@halley.astronomy.osu.edu>
2019-11-22 16:19:07 -0500

    Added 'square' flag to make plots with square panels.  needs work to get good limits

--- mplfuncs.py
+++ mplfuncs.py
@@ -22,21 +22,27 @@ def prune_y(ax,which):
     ax.yaxis.set_major_locator(MaxNLocator(nbins=len(ax.get_yticklabels())-1,\
                                            prune=which))
 
-def resize(fig,axarr,polar=False):
+def resize(fig,axarr,polar=False,square=False):
   nr,nc = np.atleast_2d(axarr).shape
   nr = nr-1
   nc = nc-1
-  Y = np.array([6,11,15,18,20,21])
-  X = 9.0*Y[nc]/6.0
-  if polar:
-    Y = X
+  if square:
+    X = 6.5 * (nc + 1)
+    Y = 6.5 * (nr + 1)
+    B = 0.15/(min((nr,nc))+1)
+    L = 0.15/(min((nr,nc))+1)
   else:
-    Y = Y[nr]
+    Y = np.array([6,11,15,18,20,21])
+    X = 9.0*Y[nc]/6.0
+    B = 0.15*6./Y
+    L = 0.125*9./X
+    if polar:
+      Y = X
+    else:
+      Y = Y[nr]
   fig.set_size_inches(X,Y)
   for ax in np.ravel(axarr):
     ax.grid(True)
     ax.minorticks_on()
-  B = 0.15*6./Y
-  L = 0.125*9./X
   fig.subplots_adjust(left=L,bottom=B)
   return fig,axarr
