5ac6e402e3ce3603290f5b554cba093d981a6605
Matthias Raives <mattjraives@gmail.com>
2020-04-14 14:21:03 -0400

    Compliance with PEP8 style

--- constants.py
+++ constants.py
@@ -1,38 +1,38 @@
-## All Units in cgs
+# All Units in cgs
 
-## ASTRONOMICAL
+# ASTRONOMICAL
 msun = 1.989e33
 rsun = 6.957e10
 lsun = 3.848e33
-au   = 1.496e13
-pc   = 3.086e18
-ly   = 9.461e17
+au = 1.496e13
+pc = 3.086e18
+ly = 9.461e17
 
 # COSMOLOGICAL
-hubble   = 2e-18 #70 km/s/Mpc
-omega_b  = 0.045
+hubble = 2e-18    # 70 km/s/Mpc
+omega_b = 0.045
 omega_dm = 0.22
-omega_l  = 0.734
+omega_l = 0.734
 
-## ATOMIC
-mp      = 1.673e-24
-me      = 9.109e-28
-mn      = 1.675e-24
-qe      = 4.803e-10
-bohr    = 5.2918e-9
+# ATOMIC
+mp = 1.673e-24
+me = 9.109e-28
+mn = 1.675e-24
+qe = 4.803e-10
+bohr = 5.2918e-9
 hplanck = 6.626e-27
-hbar    = 1.055e-27
-sigmat  = 6.652e-25
+hbar = 1.055e-27
+sigmat = 6.652e-25
 
-## RELATIVISTIC
-ggrav  = 6.674e-8
+# RELATIVISTIC
+ggrav = 6.674e-8
 clight = 2.998e10
 
-## THERMODYNAMIC
-kb     = 1.381e-16
-avogn  = 6.022e23
-gasr   = 8.314e7
+# THERMODYNAMIC
+kb = 1.381e-16
+avogn = 6.022e23
+gasr = 8.314e7
 
-## RADIATION
-arad   = 7.56e-15
+# RADIATION
+arad = 7.56e-15
 sigmab = 5.670e-5
